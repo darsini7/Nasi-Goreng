159375b960a9813443ac60d3f10117fd6f993a09
darsini7 <darsini717@gmail.com>
2022-10-06 11:55:53 +0700

    Nasi goreng mantul

---
+++ Resep.java
@@ -0,0 +1,12 @@
+public class resep {
+    
+    public static void main(String[] args) {
+        System.out.println("Resep Nasi goreng");
+        System.out.println("-----------------");
+        System.out.println("1. Nasi");
+        System.out.println("2. Telur");
+        System.out.println("3. Bawang Merah");
+        System.out.println("4. Bawang Putih");
+        System.out.println("5. Kecap");
+    }
+}
